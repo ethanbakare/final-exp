0c24f2130037cf68990cadb3a088cd81d0028fc7
ethanbakare <iamethanbakare@gmail.com>
2025-04-15 13:51:36 +0100

    Fix: commented out WebSocket cleanup to resolve TypeScript error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,14 @@ import type { AppProps } from "next/app";
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    return () => {
-      // Cleanup WebSocket server on app shutdown
-      if (global.wss) {
-        global.wss.close()
-      }
-    }
-  }, [])
+  // useEffect(() => {
+  //   return () => {
+  //     // Cleanup WebSocket server on app shutdown
+  //     if (global.wss) {
+  //       global.wss.close()
+  //     }
+  //   }
+  // }, [])
 
   return <Component {...pageProps} />;
 }
