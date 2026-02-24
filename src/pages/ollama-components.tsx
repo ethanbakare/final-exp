ac5f811b6ae2e758f290f586a640c4d83c472735
ethanbakare <iamethanbakare@gmail.com>
2026-02-24 00:46:35 +0000

    Revert "Add 615x654 holder div around each expression image"
    This reverts commit 7e1e0eff35d0a5e8be695b4f48ed9e4c113adaf0.

--- src/pages/ollama-components.tsx
+++ src/pages/ollama-components.tsx
@@ -43,13 +43,11 @@ export default function OllamaComponentsPage() {
           <div className={styles['ollama-expression-grid']}>
             {expressions.map((expr) => (
               <div key={expr.file} className={styles['ollama-expression-card']}>
-                <div className={styles['ollama-expression-img-holder']}>
-                  <img
-                    src={`/images/ollama/${expr.file}.webp`}
-                    alt={`Ollama ${expr.name} expression`}
-                    className={styles['ollama-expression-img']}
-                  />
-                </div>
+                <img
+                  src={`/images/ollama/${expr.file}.webp`}
+                  alt={`Ollama ${expr.name} expression`}
+                  className={styles['ollama-expression-img']}
+                />
                 <span className={styles['ollama-expression-label']}>{expr.name}</span>
               </div>
             ))}
