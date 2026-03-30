36668f1b255f8b09a2436714329263ed60754375
ethanbakare <iamethanbakare@gmail.com>
2026-03-30 12:38:30 +0100

    Scale Voice UI blob image to fill card with object-fit cover

--- src/projects/new-home/components/CarouselDemos.tsx
+++ src/projects/new-home/components/CarouselDemos.tsx
@@ -158,9 +158,11 @@ const CarouselDemos: React.FC = () => {
         }
 
         .container :global(.voice-blob) {
-          width: 65%;
-          height: auto;
-          object-fit: contain;
+          position: absolute;
+          inset: 0;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
           pointer-events: none;
           user-select: none;
         }
